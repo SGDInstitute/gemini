8aca3d05db66dbab5827e9b80bdad4de339ddc72
Andy Swick <andymswick@gmail.com>
2019-12-27 22:31:08 -0600

    Update module name to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { View, StyleSheet, Text, Platform } from "react-native";
 import { Header } from 'react-native-elements';
-import Constants from 'expo-constants';
 
-const CustomHeader = props => (
+const NavBar = props => (
     <Header
         leftComponent={{ icon: 'menu', color: '#fff', onPress: () => props.navigation.openDrawer(), }}
         centerComponent={{ text: props.title, style: { color: '#fff', fontSize: 20, fontWeight: "700" } }}
@@ -14,18 +12,4 @@ const CustomHeader = props => (
     />
 );
 
-export default CustomHeader;
-
-
-const statusHeight = (Platform.OS === 'ios' ? Constants.statusBarHeight : 0);
-
-const styles = StyleSheet.create({
-    container: {
-        // height: 80,
-        // flex: 1,
-        // flexDirection: 'row',
-        paddingTop: statusHeight + 20,
-        color: '#fff',
-        backgroundColor: '#009999',
-    }
-});
+export default NavBar;
