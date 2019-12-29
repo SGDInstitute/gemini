c82b34913da7574cbd5e9a27ea798d2980cb88fb
Andy Swick <andymswick@gmail.com>
2019-12-29 11:12:59 -0600

    Remove nav from Activity for now

--- src/screens/Schedule/Activity.js
+++ src/screens/Schedule/Activity.js
@@ -5,15 +5,11 @@ import NavBar from '../../components/NavBar';
 
 export default class Activity extends React.Component {
     render() {
-        const { navigation } = this.props;
         return (
             <View style={styles.flex1}>
                 <NavBar title="Activity" />
                 <View style={styles.container}>
                     <Text>Activity Screen</Text>
-                    <Text>
-                        itemId: {JSON.stringify(navigation.getParam('activityId', 'NO-ID'))}
-                    </Text>
                 </View>
             </View>
         );
