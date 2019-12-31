fa29f69f06e29cc674af3da46602f6345b0d1252
Andy Swick <andymswick@gmail.com>
2019-12-31 17:51:23 -0600

    Add maps

--- src/screens/Maps.js
+++ src/screens/Maps.js
@@ -1,18 +1,55 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
 
-import NavBar from '../components/NavBar';
-import styles from "./styles";
+import NavBar from '../components/NavBar'
+
+import markers from '../../assets/data/locations.json';
 
 export default class Maps extends React.Component {
+    onRegionChange = () => { }
+
+    renderMarkers = () => {
+        return markers.map(marker => (
+            <Marker
+                key={marker.id}
+                coordinate={marker.latlng}
+                title={marker.title}
+                description={marker.description}
+            />
+        ));
+    }
+
     render() {
         return (
-            <View style={styles.flex1}>
+            <View>
                 <NavBar title="Maps" />
-                <View style={styles.container}>
-                    <Text>Maps Screen</Text>
-                </View>
+                <MapView
+                    initialRegion={{
+                        latitude: 42.384190,
+                        longitude: -85.614535,
+                        latitudeDelta: 0.001,
+                        longitudeDelta: 0.009,
+                    }}
+                    style={styles.mapStyle}
+                >
+                    {this.renderMarkers()}
+                </MapView>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    mapStyle: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+});
