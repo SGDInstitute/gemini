8f9e8c3b06d6ec4c35e392851b900fcc36eba271
Andy Swick <andymswick@gmail.com>
2020-02-01 12:05:01 -0600

    Add policies

--- src/screens/About.js
+++ src/screens/About.js
@@ -7,6 +7,29 @@ import Constants from 'expo-constants';
 
 import NavBar from '../components/NavBar';
 
+const policies = [
+    {
+        title: 'Code for Inclusion',
+        icon: 'all-inclusive',
+        link: 'https://apps.sgdinstitute.org/events/mblgtacc-2020/policies/inclusion'
+    },
+    {
+        title: 'Photo',
+        icon: 'camera',
+        link: 'https://apps.sgdinstitute.org/events/mblgtacc-2020/policies/photo'
+    },
+    {
+        title: 'Refund',
+        icon: 'credit-card-refund',
+        link: 'https://apps.sgdinstitute.org/events/mblgtacc-2020/policies/refund'
+    },
+    {
+        title: 'Privacy',
+        icon: 'fingerprint',
+        link: 'https://sgdinstitute.org/legal-privacy'
+    },
+];
+
 const mblgtacc = [
     {
         title: 'Facebook',
@@ -22,6 +45,11 @@ const mblgtacc = [
         title: 'Instagram',
         icon: 'instagram',
         link: 'https://instagram.com/mblgtacc'
+    },
+    {
+        title: 'Website',
+        icon: 'monitor-dashboard',
+        link: 'https://mblgtacc.org'
     },
 ];
 
@@ -56,6 +84,11 @@ const sgdinstitute = [
         icon: 'linkedin',
         link: 'https://www.linkedin.com/company/12175681?trk=tyah&trkInfo=clickedVertical%3Acompany%2CclickedEntityId%3A12175681%2Cidx%3A2-1-2%2CtarId%3A1472680024853%2Ctas%3AMidwest%20Institute%20for%20Se'
     },
+    {
+        title: 'Website',
+        icon: 'monitor-dashboard',
+        link: 'https://sgdinstitute.org'
+    },
 ];
 
 export default class About extends React.PureComponent {
@@ -66,8 +99,24 @@ export default class About extends React.PureComponent {
             <View style={[t.flex1]}>
                 <NavBar title="About" />
                 <ScrollView>
-                    <Text style={[t.pX4, t.pY8]}>The Midwest Bisexual Lesbian Gay Transgender Asexual College Conference (MBLGTACC) is America's largest and oldest continuously-running conference for queer and trans+ college students. “MBLGTACC,” “Midwest Bisexual Lesbian Gay Transgender Asexual College Conference,” “Midwest Bisexual Lesbian Gay Transgender Ally College Conference,” and the MBLGTACC logo are servicemarks of the Midwest Institute for Sexuality and Gender Diversity, a non-profit 501(c)(3) organization. Our Tax ID is: 81-1788851.</Text>
+                    <Text style={[t.pX4, t.pY8]}>The Midwest Bisexual Lesbian Gay Transgender Asexual College Conference (MBLGTACC) is America's largest and oldest continuously-running conference for queer and trans+ college students. “MBLGTACC,” “Midwest Bisexual Lesbian Gay Transgender Asexual College Conference,” “Midwest Bisexual Lesbian Gay Transgender Ally College Conference,” and the MBLGTACC logo are servicemarks of the Midwest Institute for Sexuality and Gender Diversity, a non-profit 501(c)(3) organization.</Text>
 
+
+                    <View>
+                        <Text style={[t.p4, t.bgGray200, t.border, t.borderGray400, t.textLg]}>Policies</Text>
+                        {
+                            policies.map((item, i) => (
+                                <ListItem
+                                    key={i}
+                                    title={item.title}
+                                    leftIcon={<MaterialCommunityIcons name={item.icon} size={28} />}
+                                    onPress={() => Linking.openURL(item.link)}
+                                    bottomDivider
+                                    chevron
+                                />
+                            ))
+                        }
+                    </View>
 
                     <View>
                         <Text style={[t.p4, t.bgGray200, t.border, t.borderGray400, t.textLg]}>MBLGTACC Social Links</Text>
