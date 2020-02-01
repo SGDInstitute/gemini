d3c6a59d3840e588966628d2e192c878dd78596b
Andy Swick <andymswick@gmail.com>
2020-01-31 20:49:53 -0600

    Remove settings, fix press color

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,9 +6,8 @@ class NavBar extends React.Component {
     render() {
         return (
             <Header
-                leftComponent={{ icon: 'menu', color: '#fff', onPress: () => this.props.navigation.openDrawer(), }}
+                leftComponent={{ icon: 'menu', color: '#fff', underlayColor: '#546163', onPress: () => this.props.navigation.openDrawer(), }}
                 centerComponent={{ text: this.props.title, style: { color: '#fff', fontSize: 20, fontWeight: "700" } }}
-                rightComponent={{ icon: 'settings', color: '#fff', onPress: () => this.props.navigation.navigate('Settings') }}
                 containerStyle={{
                     backgroundColor: '#009999',
                 }}
